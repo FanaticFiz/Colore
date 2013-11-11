f7adcc29c691fef9d009697f1750c6805bff8dfb
FanaticFiz <Fanatic83@gmail.com>
2013-11-11 22:35:24 +0200

    xml

--- ColoreMaze/src/com/example/test1/Levels.java
+++ ColoreMaze/src/com/example/test1/Levels.java
@@ -1,5 +1,7 @@
 package com.example.test1;
 
+import java.util.ArrayList;
+import org.xmlpull.v1.XmlPullParser;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -7,6 +9,7 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class Levels extends Activity {
@@ -18,6 +21,31 @@ public class Levels extends Activity {
 		
 		GridView gridview = (GridView) findViewById(R.id.levelselect);
 	    gridview.setAdapter(new gridAdapter_LevelsImage(this));
+	    
+	    
+	    // ------------------- Достаем из XML уровень -------------------------
+	    ArrayList<String> list = new ArrayList<String>();
+        
+        try {
+            XmlPullParser parser = getResources().getXml(R.xml.contacts);
+
+            while (parser.getEventType()!= XmlPullParser.END_DOCUMENT) {
+                if (parser.getEventType() == XmlPullParser.START_TAG
+                        && parser.getName().equals("array-list")) 
+                {
+                    list.add(parser.getAttributeValue(0));
+                    list.add(parser.getAttributeValue(1));
+                    list.add(parser.getAttributeValue(2));
+                    list.add(parser.getAttributeValue(3));
+                }
+                parser.next();
+            }
+        }
+        catch (Throwable t) {
+            Toast.makeText(this,"Ошибка при загрузке XML-документа: " + t.toString(), 4000).show();
+        }
+	    // ------------------- Достаем из XML уровень -------------------------
+        	    
 	    
 	    
 	    gridview.setOnItemClickListener(new OnItemClickListener() 
