0420acabd88899ab1dbef2d057028f36c7301892
FanaticFiz <fanatic83@gmail.com>
2013-11-12 14:54:39 +0200

    XML work

--- ColoreMaze/src/com/example/test1/Levels.java
+++ ColoreMaze/src/com/example/test1/Levels.java
@@ -1,7 +1,10 @@
 package com.example.test1;
 
 import java.util.ArrayList;
+
+import org.xml.sax.Parser;
 import org.xmlpull.v1.XmlPullParser;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -14,6 +17,8 @@ import android.widget.AdapterView.OnItemClickListener;
 
 public class Levels extends Activity {
 	
+	ArrayList<String> list = new ArrayList<String>();
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,45 +26,86 @@ public class Levels extends Activity {
 		
 		GridView gridview = (GridView) findViewById(R.id.levelselect);
 	    gridview.setAdapter(new gridAdapter_LevelsImage(this));
-	    
-	    
-	    // ------------------- Достаем из XML уровень -------------------------
-	    ArrayList<String> list = new ArrayList<String>();
-        
-        try {
-            XmlPullParser parser = getResources().getXml(R.xml.contacts);
-
-            while (parser.getEventType()!= XmlPullParser.END_DOCUMENT) {
-                if (parser.getEventType() == XmlPullParser.START_TAG
-                        && parser.getName().equals("array-list")) 
-                {
-                    list.add(parser.getAttributeValue(0));
-                    list.add(parser.getAttributeValue(1));
-                    list.add(parser.getAttributeValue(2));
-                    list.add(parser.getAttributeValue(3));
-                }
-                parser.next();
-            }
-        }
-        catch (Throwable t) {
-            Toast.makeText(this,"Ошибка при загрузке XML-документа: " + t.toString(), 4000).show();
-        }
-	    // ------------------- Достаем из XML уровень -------------------------
-        	    
-	    
+	     
 	    
 	    gridview.setOnItemClickListener(new OnItemClickListener() 
 	    {
 	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) 
 	        {
+	        	ChoiseLevel(position);
 	        	// Прыгаем в Игру
 	        	Intent intentG = new Intent();
 	    		intentG.setClass(Levels.this, game_gameActivity.class);
-	    		startActivity(intentG);	        	
+	    		startActivity(intentG);	  
 	        }
 	    });			
 	}
 
+	
+	// Определяем какой файл xml подгрузить в зависимости от выбора уровня...
+	public void ChoiseLevel(int NP) 
+	{
+		String StringNP;
+		int i;
+		switch (NP) {
+		case 0:
+			i = NP+1;
+			StringNP = "level"+i;
+			ParserXML(StringNP);
+			break;
+			
+		case 1:
+			i = NP+1;
+			StringNP = "level"+i;
+			ParserXML(StringNP);		
+			break;
+
+		case 2:
+			i = NP+1;
+			StringNP = "level"+i;
+			ParserXML(StringNP);			
+			break;
+
+		default:
+			break;
+		}
+	}
+	
+	
+	private void ParserXML(String Name_of_XML_file) 
+	{
+		 // ------------------- Достаем из XML уровень -------------------------
+	    // и загоняем в массив list
+		
+		// Получаем ID нужного файла исполльзуя название файла
+	    int XMLFileID = getResources().getIdentifier(Name_of_XML_file,"xml",getPackageName());
+			    
+        try 
+        {
+        XmlPullParser parser = getResources().getXml(XMLFileID);
+
+        	while (parser.getEventType()!= XmlPullParser.END_DOCUMENT) 
+        	{
+        		if (parser.getEventType() == XmlPullParser.START_TAG
+                    && parser.getName().equals("array-list")) 
+        		{
+        			for (int i = 0; i < parser.getAttributeCount(); i++)
+        			{
+        				list.add(parser.getAttributeValue(i));
+        			}
+        		}
+        	parser.next();
+        	}
+        }
+        
+        catch (Throwable t) 
+        {
+            Toast.makeText(this,"Ошибка при загрузке XML-документа: " + t.toString(), 4000).show();
+        }
+	    // ------------------- Достаем из XML уровень -------------------------    
+	    
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
