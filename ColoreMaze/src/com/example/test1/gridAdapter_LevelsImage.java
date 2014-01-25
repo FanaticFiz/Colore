0a7a5ee0632de0d6817f9f84c868aeb0690daee9
FanaticFiz <Fanatic83@gmail.com>
2014-01-25 18:54:12 +0200

    логика для отображения пройденных уровней. Ох как ребенок мешает работать )))

--- ColoreMaze/src/com/example/test1/gridAdapter_LevelsImage.java
+++ ColoreMaze/src/com/example/test1/gridAdapter_LevelsImage.java
@@ -23,7 +23,7 @@ public class gridAdapter_LevelsImage extends BaseAdapter {
     // этом методе поидее надо смотреть чему равна переменная type_of_game и тогда уже брать длинну 
     // соответсвующего массива mThumbIds[] 
     // НО у меня поидее эти массивы будут одинаковый длинны все, поэтому мы берем длинну одного из них только.
-    public int getCount() 						{ 	return mThumbIds1.length-12;   }
+    public int getCount() 						{ 	return mThumbIds1.length-24;   }
     public Object getItem(int position) 		{ 	return null;				}
     public long getItemId(int position) 		{ 	return 0;	   				}
 
@@ -50,41 +50,74 @@ public class gridAdapter_LevelsImage extends BaseAdapter {
         // возможно даже что так лучше в плане что ненужно в коде потом менять состояния или как то рисовать/перерисовывать что то
         switch (position) 
         {
-		case 0:	ggggg = position+12;		break;
-				
-		case 1:	if (array_of_P[0] > 0) 	{  	ggggg = position + 12;	}
-				else 					{	ggggg = position;		}
+		case 0:	if (array_of_P[0]==0) 	{	ggggg = position+12;	}
+				if (array_of_P[0]>0) 	{	ggggg = position+24;	}
 				break;
-		case 2:	if (array_of_P[1] > 0) 	{  	ggggg = position + 12;	}
-				else 					{	ggggg = position;		}
-				break;
-		case 3:	if (array_of_P[2] > 0)	{  	ggggg = position + 12;	}
-				else 					{	ggggg = position;		}
-				break;
-		case 4:	if (array_of_P[3] > 0) 	{  	ggggg = position + 12;	}
-				else 					{	ggggg = position;		}
-				break;
-		case 5:	if (array_of_P[4] > 0)	{  	ggggg = position + 12;	}
-				else 					{	ggggg = position;		}
-				break;
-		case 6:	if (array_of_P[5] > 0)	{  	ggggg = position + 12;	}
-				else 					{	ggggg = position;		}
-				break;
-		case 7:	if (array_of_P[6] > 0)	{  	ggggg = position + 12;	}
-				else 					{	ggggg = position;		}
-				break;
-		case 8:	if (array_of_P[7] > 0)	{  	ggggg = position + 12;	}
-				else 					{	ggggg = position;		}
-				break;
-		case 9:	if (array_of_P[8] > 0)	{  	ggggg = position + 12;	}
-				else 					{	ggggg = position;		}
-				break;
-		case 10:if (array_of_P[9]> 0)	{  	ggggg = position + 12;	}
-				else 					{	ggggg = position;		}
-				break;
-		case 11:if (array_of_P[10]> 0)	{  	ggggg = position + 12;	}
-				else 					{	ggggg = position;		}
-				break;
+
+		case 1:
+			if 	(array_of_P[0]== 0) 					{  	ggggg = position;		}	// замочек
+			if ((array_of_P[0] > 0)&(array_of_P[1]==0)) {  	ggggg = position + 12;	}	// чисто
+			if  (array_of_P[1] > 0)						{	ggggg = position + 24;	}	// галочка			
+			break;
+		case 2:	
+			if 	(array_of_P[1]== 0) 					{  	ggggg = position;		}
+			if ((array_of_P[1] > 0)&(array_of_P[2]==0)) {  	ggggg = position + 12;	}
+			if  (array_of_P[2] > 0)						{	ggggg = position + 24;	}	// галочка				
+			break;
+
+		case 3:				
+			if 	(array_of_P[2]== 0) 					{  	ggggg = position;		}
+			if ((array_of_P[2] > 0)&(array_of_P[3]==0)) {  	ggggg = position + 12;	}
+			if  (array_of_P[3] > 0)						{	ggggg = position + 24;	}	// галочка				
+			break;
+
+		case 4:	
+			if 	(array_of_P[3]== 0) 					{  	ggggg = position;		}
+			if ((array_of_P[3] > 0)&(array_of_P[4]==0)) {  	ggggg = position + 12;	}
+			if  (array_of_P[4] > 0)						{	ggggg = position + 24;	}	// галочка				
+			break;
+
+		case 5:	
+			if 	(array_of_P[4]== 0) 					{  	ggggg = position;		}
+			if ((array_of_P[4] > 0)&(array_of_P[5]==0)) {  	ggggg = position + 12;	}
+			if  (array_of_P[5] > 0)						{	ggggg = position + 24;	}	// галочка				
+			break;
+
+		case 6:	
+			if 	(array_of_P[5]== 0) 					{  	ggggg = position;		}
+			if ((array_of_P[5] > 0)&(array_of_P[6]==0)) {  	ggggg = position + 12;	}
+			if  (array_of_P[6] > 0)						{	ggggg = position + 24;	}	// галочка				
+			break;
+
+		case 7:	
+			if 	(array_of_P[6]== 0) 					{  	ggggg = position;		}
+			if ((array_of_P[6] > 0)&(array_of_P[7]==0)) {  	ggggg = position + 12;	}
+			if  (array_of_P[7] > 0)						{	ggggg = position + 24;	}	// галочка				
+			break;
+
+		case 8:	
+			if 	(array_of_P[7]== 0) 					{  	ggggg = position;		}
+			if ((array_of_P[7] > 0)&(array_of_P[8]==0)) {  	ggggg = position + 12;	}
+			if  (array_of_P[8] > 0)						{	ggggg = position + 24;	}	// галочка				
+			break;
+
+		case 9:	
+			if 	(array_of_P[8]== 0) 					{  	ggggg = position;		}
+			if ((array_of_P[8] > 0)&(array_of_P[9]==0)) {  	ggggg = position + 12;	}
+			if  (array_of_P[9] > 0)						{	ggggg = position + 24;	}	// галочка				
+			break;
+
+		case 10:
+			if 	(array_of_P[9]== 0) 					{  	ggggg = position;		}
+			if ((array_of_P[9] > 0)&(array_of_P[10]==0)){  	ggggg = position + 12;	}
+			if  (array_of_P[10]> 0)						{	ggggg = position + 24;	}	// галочка				
+			break;
+
+		case 11:
+			if 	(array_of_P[10]== 0) 						{  	ggggg = position;		}
+			if ((array_of_P[10] > 0)&(array_of_P[11]==0)) 	{  	ggggg = position + 12;	}
+			if  (array_of_P[11] > 0)						{	ggggg = position + 24;	}	// галочка				
+			break;
 
 		default:
 			ggggg = position;
@@ -118,13 +151,17 @@ public class gridAdapter_LevelsImage extends BaseAdapter {
     // в активити levels. На данный момент я планирую 24 уровня на каждый тип игры.
     private Integer[] mThumbIds1 = {
     		
-    		R.drawable.layer_of_levelsgrid1,	R.drawable.layer_of_levelsgrid2,	R.drawable.layer_of_levelsgrid3,	R.drawable.layer_of_levelsgrid4,
+    		R.drawable.layer_of_levelsgrid1,	R.drawable.layer_of_levelsgrid2,	R.drawable.layer_of_levelsgrid3,	R.drawable.layer_of_levelsgrid4,		// замочек	
             R.drawable.layer_of_levelsgrid5,	R.drawable.layer_of_levelsgrid6,	R.drawable.layer_of_levelsgrid7,	R.drawable.layer_of_levelsgrid8,
             R.drawable.layer_of_levelsgrid9,	R.drawable.layer_of_levelsgrid10,	R.drawable.layer_of_levelsgrid11,	R.drawable.layer_of_levelsgrid12,
               
-    		R.drawable.type1_level1,			R.drawable.type1_level2,			R.drawable.type1_level3,			R.drawable.type1_level4,
+    		R.drawable.type1_level1,			R.drawable.type1_level2,			R.drawable.type1_level3,			R.drawable.type1_level4,				// чисто
     		R.drawable.type1_level5,			R.drawable.type1_level6,			R.drawable.type1_level7,			R.drawable.type1_level8,
-    		R.drawable.type1_level9,			R.drawable.type1_level10,			R.drawable.type1_level11,			R.drawable.type1_level12,          
+    		R.drawable.type1_level9,			R.drawable.type1_level10,			R.drawable.type1_level11,			R.drawable.type1_level12,
+    		
+    		R.drawable.layer_of_levelsgrid1_ok,	R.drawable.layer_of_levelsgrid2_ok,	R.drawable.layer_of_levelsgrid3_ok,	R.drawable.layer_of_levelsgrid4_ok,		// галочка
+            R.drawable.layer_of_levelsgrid5_ok,	R.drawable.layer_of_levelsgrid6_ok,	R.drawable.layer_of_levelsgrid7_ok,	R.drawable.layer_of_levelsgrid8_ok,
+            R.drawable.layer_of_levelsgrid9_ok,	R.drawable.layer_of_levelsgrid10_ok,R.drawable.layer_of_levelsgrid11_ok,R.drawable.layer_of_levelsgrid12_ok,
     };
     
     private Integer[] mThumbIds2 = {
